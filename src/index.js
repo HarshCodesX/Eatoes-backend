9d69586f268336fcbc9d567e1639dcc740e6cf8a
Harsh <Harshguptadm7562@gmail.com>
2025-04-20 23:17:25 +0530

    files added

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const { connectDB } = require("./config/db")
+require("dotenv").config()
+
+const menuRoutes = require("./routes/menuRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+app.use(cors({
+    "origin": ["http://localhost:5173"],
+    "method": ["GET", "POST", "PUT", "PATCH"],
+    credentials: true
+}))
+app.use(express.json())
+
+app.use("/menu", menuRoutes)
+app.use("/order", orderRoutes)
+app.get("/", (req, res) => {
+    res.send("API is running...")
+})
+
+connectDB()
+.then(() => {
+    console.log("Database connection established...");
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is sucessfully running on PORT ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log("DATABASE can not be connected")
+});
